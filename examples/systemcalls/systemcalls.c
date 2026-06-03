2877634995dfdd31a8e158c69bcaeec713b2a469
Kuldeep Nigam <KuldeepNigam@eaton.com>
2026-06-03 11:00:20 +0000

    Updated  assignment-3-part-1

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,10 @@
 #include "systemcalls.h"
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
 
 /**
  * @param cmd the command to execute with system()
@@ -17,7 +23,16 @@ bool do_system(const char *cmd)
  *   or false() if it returned a failure
 */
 
-    return true;
+    if (cmd == NULL) {
+        return false;
+    }
+
+    int status = system(cmd);
+    if (status == -1) {
+        return false;
+    }
+
+    return WIFEXITED(status) && (WEXITSTATUS(status) == 0);
 }
 
 /**
@@ -36,6 +51,10 @@ bool do_system(const char *cmd)
 
 bool do_exec(int count, ...)
 {
+    if (count < 1) {
+        return false;
+    }
+
     va_list args;
     va_start(args, count);
     char * command[count+1];
@@ -58,10 +77,24 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+   va_end(args);
 
-    va_end(args);
+    pid_t pid = fork();
+    if (pid < 0) {
+        return false;
+    }
 
-    return true;
+    if (pid == 0) {
+        execv(command[0], command);
+        exit(EXIT_FAILURE);
+    }
+
+    int status;
+    if (waitpid(pid, &status, 0) < 0) {
+        return false;
+    }
+
+    return WIFEXITED(status) && (WEXITSTATUS(status) == 0);
 }
 
 /**
@@ -71,6 +104,10 @@ bool do_exec(int count, ...)
 */
 bool do_exec_redirect(const char *outputfile, int count, ...)
 {
+    if ((outputfile == NULL) || (count < 1)) {
+        return false;
+    }
+
     va_list args;
     va_start(args, count);
     char * command[count+1];
@@ -84,7 +121,6 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     // and may be removed
     command[count] = command[count];
 
-
 /*
  * TODO
  *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
@@ -95,5 +131,31 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
 
     va_end(args);
 
-    return true;
+    pid_t pid = fork();
+    if (pid < 0) {
+        return false;
+    }
+
+    if (pid == 0) {
+        int fd = open(outputfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if (fd < 0) {
+            exit(EXIT_FAILURE);
+        }
+
+        if (dup2(fd, STDOUT_FILENO) < 0) {
+            close(fd);
+            exit(EXIT_FAILURE);
+        }
+
+        close(fd);
+        execv(command[0], command);
+        exit(EXIT_FAILURE);
+    }
+
+    int status;
+    if (waitpid(pid, &status, 0) < 0) {
+        return false;
+    }
+
+    return WIFEXITED(status) && (WEXITSTATUS(status) == 0);
 }
